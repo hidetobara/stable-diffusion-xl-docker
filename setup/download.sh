24a858813217965473f9e50f65f4081710538017
Hideto Obara <hidetobara@gmail.com>
2023-08-27 00:19:03 +0900

    fix legacy.

--- setup/download.sh
+++ setup/download.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# SDXL base rquired
 wget -nc -P "models/checkpoints/" "https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors"
+# SDXL vae not required
 wget -nc -P "models/checkpoints/" "https://huggingface.co/stabilityai/sdxl-vae/resolve/main/sdxl_vae.safetensors"
 
+# for Control-Net
 wget -nc -P "models/controlnet/" "https://huggingface.co/diffusers/controlnet-canny-sdxl-1.0/resolve/main/diffusion_pytorch_model.fp16.safetensors"
 
 echo "Ready to run sdxl !"
